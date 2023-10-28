674d50d62b245d5bf4f71d47ea297b474ad42ffa
WeberLucas <weber13lucas@gmail.com>
2023-10-27 22:02:03 -0300

    primeiro exercicio aula 7. verificando se existe a tag target

--- cypress/e2e/Teste-doTeste.spec.cy.js
+++ cypress/e2e/Teste-doTeste.spec.cy.js
@@ -149,7 +149,9 @@ it('seleciona um arquivo utilizando uma fixture para a qual foi dada um alias', 
   .should(function($input) {
   expect($input[0].files[0].name).to.equal('example.json')
   })
-
-
 })
+it.only('verifica que a política de privacidade abre em outra aba sem a necessidade de um clique', function(){
+  cy.get('#privacy a').should('have.attr','target','_blank')
+})
+
 });
