9e082ddcbd71779fbba5762e69df9f2d30ee9102
WeberLucas <weber13lucas@gmail.com>
2023-10-30 17:05:41 -0300

    aula 8 rodando os tests em tamanho mobile

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,9 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
-  e2e: {
+  e2e: {      trashAssetsBeforeRuns: false,
+
     setupNodeEvents(on, config) {
+      
       // implement node event listeners here
     },
   },
