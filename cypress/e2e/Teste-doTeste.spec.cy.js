91b4a13cc4f8135bed653153350c639966e21f9c
WeberLucas <weber13lucas@gmail.com>
2023-11-07 11:41:23 -0300

    correção de teste Seleciona um Arquivo da pasta Fixtures

--- cypress/e2e/Teste-doTeste.spec.cy.js
+++ cypress/e2e/Teste-doTeste.spec.cy.js
@@ -126,7 +126,8 @@ it('marca ambos checkboxes, depois desmarca o último',function() {
 it('seleciona um arquivo da pasta fixtures', function(){
 cy.get('input[type="file"]')
 .should('not.have.value')
-.selectFile('/home/tagplus/Área de Trabalho/weber-testes/cypress/fixtures/example.json')
+//.selectFile('/home/tagplus/Área de Trabalho/weber-testes/cypress/fixtures/example.json')
+.selectFile('cypress/fixtures/example.json')
 .should(function($input ) {
  expect ($input[0].files[0].name).to.equal('example.json')
 })
