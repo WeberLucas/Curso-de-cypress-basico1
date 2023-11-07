46bb5625a0f20c8143ad937323f67c9cb6817b15
WeberLucas <weber13lucas@gmail.com>
2023-11-07 11:38:38 -0300

    alterando a referencia do arquivo json

--- cypress/e2e/Teste-doTeste.spec.cy.js
+++ cypress/e2e/Teste-doTeste.spec.cy.js
@@ -135,7 +135,9 @@ cy.get('input[type="file"]')
 it('seleciona um arquivo simulando um drag-and-drop', function() {
   cy.get('input[type="file"]')
   .should('not.have.value')
-  .selectFile('/home/tagplus/Área de Trabalho/weber-testes/cypress/fixtures/example.json', {action: 'drag-drop'})
+  //.selectFile('/home/tagplus/Área de Trabalho/weber-testes/cypress/fixtures/example.json', {action: 'drag-drop'})
+  .selectFile('cypress/fixtures/example.json', {action: 'drag-drop'})
+
   .should(function($input ) {
    expect ($input[0].files[0].name).to.equal('example.json')
 
